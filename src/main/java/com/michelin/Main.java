47d49103cb67846b24896539ca251956b019b15f
Mikey-F6 <mgi1010@alu.ubu.es>
2025-03-24 19:19:02 +0100

    Probando

--- src/main/java/com/michelin/Main.java
+++ src/main/java/com/michelin/Main.java
@@ -34,7 +34,6 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-	//borrar
     private AbstractOptimization optimizationMethod;
     private Label tireCountLabel;
     private final Label occupancyLabel = new Label("Ocupación: 0%");
